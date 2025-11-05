4eb9cbe16408a7a684f7ea151bf079bc9675d72e
Martin-Leff <mleff@wrallp.com>
2025-11-05 09:31:50 -0500

    Starting array

--- Advent of Code/2015/aoc_2015_6.py
+++ Advent of Code/2015/aoc_2015_6.py
@@ -6,18 +6,26 @@ resp = requests.get(url)
 input_text = resp.text
 input_text = input_text.splitlines()
 
-print(input_text)
-
 # Part 1
 
 def toggle_func(coordx1, coordy1, coordx2, coordy2):
-    return
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            if d[(x, y)] == 0:
+                d.update({(x, y): 1})
+            else:
+                d.update({(x, y): 0})
 
 def turn_on(coordx1, coordy1, coordx2, coordy2):
-    return
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            d.update({(x, y): 1})
+
 
 def turn_off(coordx1, coordy1, coordx2, coordy2):
-    return
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            d.update({(x, y): 0})
 
 def sort(string):
     toggle = 'toggle'
@@ -25,10 +33,17 @@ def sort(string):
     off = 'turn off'
 
     nums = re.findall(r'\d+\.?\d*', string)
-    x1, y1, x2, y2 = int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3])
+    x1, y1, x2, y2 = int(nums[0]), int(nums[1]), int(nums[2]) + 1, int(nums[3]) + 1
 
     if string.startswith(toggle):
         toggle_func(x1, y1, x2, y2)
+    elif string.startswith(on):
+        turn_on(x1, y1, x2, y2)
+    elif string.startswith(off):
+        turn_off(x1, y1, x2, y2)
+    else:
+        print(string)
+        print('Error: Check String')
 
 d = dict()
 
@@ -36,7 +51,73 @@ for x in range(1000):
     for y in range(1000):
         d.update({(x, y): 0})
 
-print(d)
-
 for string in input_text:
     sort(string)
+
+total = 0
+
+for key in d:
+    if d[key] == 1:
+        total += 1
+
+print('Part 1 Answer:', total)
+
+# Part 2
+
+def toggle_func_2(coordx1, coordy1, coordx2, coordy2):
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            value = d[(x, y)]
+            d.update({(x, y): value + 2})
+
+
+def turn_on_2(coordx1, coordy1, coordx2, coordy2):
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            value = d[(x, y)]
+            d.update({(x, y): value + 1})
+
+
+def turn_off_2(coordx1, coordy1, coordx2, coordy2):
+    for x in range(coordx1, coordx2):
+        for y in range(coordy1, coordy2):
+            value = d[(x, y)]
+            if value == 0:
+                continue
+            else:
+                d.update({(x, y): value - 1})
+
+def sort_2(string):
+    toggle = 'toggle'
+    on = 'turn on'
+    off = 'turn off'
+
+    nums = re.findall(r'\d+\.?\d*', string)
+    x1, y1, x2, y2 = int(nums[0]), int(nums[1]), int(nums[2]) + 1, int(nums[3]) + 1
+
+    if string.startswith(toggle):
+        toggle_func_2(x1, y1, x2, y2)
+    elif string.startswith(on):
+        turn_on_2(x1, y1, x2, y2)
+    elif string.startswith(off):
+        turn_off_2(x1, y1, x2, y2)
+    else:
+        print(string)
+        print('Error: Check String')
+
+d = dict()
+
+for x in range(1000):
+    for y in range(1000):
+        d.update({(x, y): 0})
+
+for string in input_text:
+    sort_2(string)
+
+total = 0
+
+for key in d:
+    val = d[key]
+    total += val
+
+print('Part 2 Answer:', total)
