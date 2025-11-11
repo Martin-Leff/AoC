a46ec507b9208f6b63202231a3df1cbb6c68d7ee
Martin-Leff <mleff@wrallp.com>
2025-11-11 11:18:03 -0500

    Answers Finalized

---
+++ Advent of Code/2015/aoc_2015_15.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2015/Inputs/aoc_2015_15_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+input_text = input_text.splitlines()
+
+# Part 1
+
+print(input_text)
