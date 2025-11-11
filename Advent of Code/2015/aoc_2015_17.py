6639c5ebfab38f121e7dfa60f2775ab1085daae1
Martin-Leff <mleff@wrallp.com>
2025-11-11 13:47:42 -0500

    aoc 17 added

---
+++ Advent of Code/2015/aoc_2015_17.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2015/Inputs/aoc_2015_16_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+input_text = input_text.splitlines()
