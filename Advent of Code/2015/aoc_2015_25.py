d1d4d6c20a7c29cf56e3821665a4b5cdc4658ae5
Martin-Leff <mleff@wrallp.com>
2025-12-03 16:52:43 -0500

    aoc 2015 complete

--- Advent of Code/2015/aoc_2015_25.py
+++ Advent of Code/2015/aoc_2015_25.py
@@ -1,7 +1,4 @@
 import requests
-from functools import reduce
-from itertools import combinations
-from operator import mul
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2015/Inputs/aoc_2015_25_input.txt'
 
@@ -11,4 +8,32 @@ input_text = input_text.splitlines()
 
 # Part 1
 
-print(input_text)
+row = int(input_text[0].split(' ')[16].strip(','))
+col = int(input_text[0].split(' ')[18].strip('.'))
+
+start_inc = col
+start_val = 20151125
+mult_val = 252533
+div_val = 33554393
+
+i = 0
+j = 1
+
+for _ in range(col):
+    i += j
+    j += 1
+
+col_iter = col
+
+for _ in range(row - 1):
+    i += col_iter
+    col_iter += 1
+
+
+cur_val = start_val
+
+for _ in range(i - 1):
+    new_val = (cur_val * mult_val) % div_val
+    cur_val = new_val
+
+print('Part 1 Answer:', cur_val)
