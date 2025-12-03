5381a774c20683ca2cb85d7d2a86e335c909b7f3
Martin-Leff <mleff@wrallp.com>
2025-12-03 16:13:32 -0500

    Answers Finalized

--- Advent of Code/2015/aoc_2015_24.py
+++ Advent of Code/2015/aoc_2015_24.py
@@ -1,4 +1,7 @@
 import requests
+from functools import reduce
+from itertools import combinations
+from operator import mul
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2015/Inputs/aoc_2015_24_input.txt'
 
@@ -8,4 +11,23 @@ input_text = input_text.splitlines()
 
 # Part 1
 
-print(input_text)
+def calc(num_of_groups):
+    target_weight = sum(input_text) / num_of_groups
+    for i in range(len(input_text)):
+        quant_en = [reduce(mul, c) for c in combinations(input_text, i) if sum(c) == target_weight]
+        if quant_en:
+            return min(quant_en)
+
+input_text = [int(s) for s in input_text]
+
+num_of_groups = 3
+
+qe_min = calc(3)
+
+print('Part 1 Answer:', qe_min)
+
+# Part 2
+
+qe_min = calc(4)
+
+print('Part 2 Answer:', qe_min)
