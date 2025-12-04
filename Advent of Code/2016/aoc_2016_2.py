c5c6fe6bf40fa58d3f7ec337e62b8492039f2aaa
Martin-Leff <mleff@wrallp.com>
2025-12-04 12:37:03 -0500

    aoc 2 added

---
+++ Advent of Code/2016/aoc_2016_2.py
@@ -0,0 +1,5 @@
+import requests
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2015/Inputs/aoc_2015_1_input.txt'
+resp = requests.get(url)
+input_text = resp.text
