aad509d2dc7add7d61128c301ebca9f59c0b7ec8
Martin-Leff <mleff@wrallp.com>
2025-12-15 13:11:02 -0500

    Answers Finalized

--- Advent of Code/2016/aoc_2016_6.py
+++ Advent of Code/2016/aoc_2016_6.py
@@ -1,5 +1,4 @@
 import requests
-import hashlib
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2016/Inputs/aoc_2016_6_input.txt'
 resp = requests.get(url)
@@ -8,4 +7,40 @@ input_text = input_text.splitlines()
 
 # Part 1
 
-print(input_text)
+string_len = len(input_text[0])
+
+password = ''
+
+for char_pos in range(string_len):
+    char_pos_dict = {}
+    for string in input_text:
+        char = string[char_pos]
+        if char in char_pos_dict:
+            cur_count = char_pos_dict[char]
+            char_pos_dict.update({char: cur_count + 1})
+        else:
+            char_pos_dict.update({char: 0})
+
+    password += max(char_pos_dict, key=char_pos_dict.get)
+
+print('Part 1 Answer:', password)
+
+# Part 2
+
+string_len = len(input_text[0])
+
+password = ''
+
+for char_pos in range(string_len):
+    char_pos_dict = {}
+    for string in input_text:
+        char = string[char_pos]
+        if char in char_pos_dict:
+            cur_count = char_pos_dict[char]
+            char_pos_dict.update({char: cur_count + 1})
+        else:
+            char_pos_dict.update({char: 0})
+
+    password += min(char_pos_dict, key=char_pos_dict.get)
+
+print('Part 2 Answer:', password)
