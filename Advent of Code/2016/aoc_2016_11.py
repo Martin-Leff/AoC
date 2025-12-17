ba8ebf72f71166362e7d3e5b34a617be0f3d2c02
Martin-Leff <mleff@wrallp.com>
2025-12-17 14:35:38 -0500

    aoc 11 started

--- Advent of Code/2016/aoc_2016_11.py
+++ Advent of Code/2016/aoc_2016_11.py
@@ -10,3 +10,34 @@ input_text = input_text.splitlines()
 # Part 1
 
 print('Part 1 Answer:', input_text)
+
+floor_list = []
+
+for line in input_text:
+    f = []
+    print(line)
+    if 'thulium generator' in line:
+        f.append(-1)
+    if 'thulium-compatible microchip' in line:
+        f.append(1)
+    if 'plutonium generator' in line:
+        f.append(-2)
+    if 'plutonium-compatible microchip' in line:
+        f.append(2)
+    if 'strontium generator' in line:
+        f.append(-3)
+    if 'strontium-compatible microchip' in line:
+        f.append(3)
+    if 'promethium generator' in line:
+        f.append(-4)
+    if 'promethium-compatible microchip' in line:
+        f.append(4)
+    if 'ruthenium generator' in line:
+        f.append(-5)
+    if 'ruthenium-compatible microchip' in line:
+        f.append(5)
+
+    floor_list.append(f)
+
+print(floor_list)
+print(sum(floor_list, []))
