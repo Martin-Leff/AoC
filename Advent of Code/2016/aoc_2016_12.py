a204d40c40b562cf922999eb681b34d212095199
Martin-Leff <mleff@wrallp.com>
2025-12-18 14:53:46 -0500

    Answers Finalized

--- Advent of Code/2016/aoc_2016_12.py
+++ Advent of Code/2016/aoc_2016_12.py
@@ -11,5 +11,158 @@ input_text = input_text.splitlines()
 
 # Part 1
 
+i = 0
+a = 0
+b = 0
+c = 0
+d = 0
 
-print('Part 2 Answer:', input_text)
+while True:
+    command = input_text[i].split(' ')
+    instruct = command[0]
+
+    if instruct == 'cpy':
+        if command[1] == 'a':
+            val = a
+        elif command[1] == 'b':
+            val = b
+        elif command[1] == 'c':
+            val = c
+        elif command[1] == 'd':
+            val = d
+        else:
+            val = int(command[1])
+
+        reg = command[2]
+
+        if reg == 'a':
+            a = val
+        elif reg == 'b':
+            b = val
+        elif reg == 'c':
+            c = val
+        else:
+            d = val
+
+    elif instruct == 'inc':
+        reg = command[1]
+        if reg == 'a':
+            a += 1
+        elif reg == 'b':
+            b += 1
+        elif reg == 'c':
+            c += 1
+        else:
+            d += 1
+
+    elif instruct == 'dec':
+        reg = command[1]
+        if reg == 'a':
+            a -= 1
+        elif reg == 'b':
+            b -= 1
+        elif reg == 'c':
+            c -= 1
+        else:
+            d -= 1
+
+    elif instruct == 'jnz':
+        reg = command[1]
+        if reg == 'a':
+            register = a
+        elif reg == 'b':
+            register = b
+        elif reg == 'c':
+            register = c
+        elif reg == 'd':
+            register = d
+        else:
+            register = int(reg)
+        val = int(command[2])
+        if register != 0:
+            i += val - 1
+
+    i += 1
+    if i >= len(input_text):
+        break
+
+print('Part 1 Answer:', a)
+
+# Part 2
+
+i = 0
+a = 0
+b = 0
+c = 1
+d = 0
+
+while True:
+    command = input_text[i].split(' ')
+    instruct = command[0]
+
+    if instruct == 'cpy':
+        if command[1] == 'a':
+            val = a
+        elif command[1] == 'b':
+            val = b
+        elif command[1] == 'c':
+            val = c
+        elif command[1] == 'd':
+            val = d
+        else:
+            val = int(command[1])
+
+        reg = command[2]
+
+        if reg == 'a':
+            a = val
+        elif reg == 'b':
+            b = val
+        elif reg == 'c':
+            c = val
+        else:
+            d = val
+
+    elif instruct == 'inc':
+        reg = command[1]
+        if reg == 'a':
+            a += 1
+        elif reg == 'b':
+            b += 1
+        elif reg == 'c':
+            c += 1
+        else:
+            d += 1
+
+    elif instruct == 'dec':
+        reg = command[1]
+        if reg == 'a':
+            a -= 1
+        elif reg == 'b':
+            b -= 1
+        elif reg == 'c':
+            c -= 1
+        else:
+            d -= 1
+
+    elif instruct == 'jnz':
+        reg = command[1]
+        if reg == 'a':
+            register = a
+        elif reg == 'b':
+            register = b
+        elif reg == 'c':
+            register = c
+        elif reg == 'd':
+            register = d
+        else:
+            register = int(reg)
+        val = int(command[2])
+        if register != 0:
+            i += val - 1
+
+    i += 1
+    if i >= len(input_text):
+        break
+
+print('Part 2 Answer:', a)
