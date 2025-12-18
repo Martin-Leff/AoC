e7555e5bcfb07c2b3d9adcf7ff17733c26b8c01a
Martin-Leff <mleff@wrallp.com>
2025-12-18 16:27:14 -0500

    Answers Finalized

--- Advent of Code/2016/aoc_2016_13.py
+++ Advent of Code/2016/aoc_2016_13.py
@@ -7,8 +7,63 @@ from itertools import chain, combinations
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2016/Inputs/aoc_2016_13_input.txt'
 resp = requests.get(url)
 input_text = resp.text
-input_text = input_text.splitlines()
 
 # Part 1
 
-print('Part 2 Answer:', input_text)
+def is_valid(x, y, offset):
+    valid = False
+    val = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + offset
+    bin_val = format(val, 'b')
+    even_odd = bin_val.count('1')
+    if even_odd % 2 == 0:
+        valid = True
+    return valid
+
+offset = int(input_text)
+end_coord = (31, 39)
+
+seen = {(1, 1)}
+steps = 0
+new = seen
+keep_looking = True
+
+while keep_looking == True:
+    check_here = new.copy()
+    new = set()
+    for old_x, old_y in check_here:
+        for new_x, new_y in [(old_x, old_y + 1), (old_x, old_y - 1), (old_x + 1, old_y), (old_x - 1, old_y)]:
+            if (new_x, new_y) in seen or not is_valid(new_x, new_y, offset) or new_x < 0 or new_y < 0:
+                continue
+            seen.add((new_x, new_y))
+            new.add((new_x, new_y))
+    steps += 1
+
+    if end_coord in seen:
+        keep_looking = False
+
+print('Part 1 Answer:', steps)
+
+# Part 2
+
+seen = {(1, 1)}
+steps = 0
+new = seen
+keep_looking = True
+
+while keep_looking == True:
+    check_here = new.copy()
+    new = set()
+    for old_x, old_y in check_here:
+        for new_x, new_y in [(old_x, old_y + 1), (old_x, old_y - 1), (old_x + 1, old_y), (old_x - 1, old_y)]:
+            if (new_x, new_y) in seen or not is_valid(new_x, new_y, offset) or new_x < 0 or new_y < 0:
+                continue
+            seen.add((new_x, new_y))
+            new.add((new_x, new_y))
+    steps += 1
+
+    if steps == 50:
+        keep_looking = False
+
+locations = len(seen)
+
+print('Part 2 Answer:', locations)
