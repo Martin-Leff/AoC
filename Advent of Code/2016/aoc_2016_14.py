fe03d70eef947f42e60703b9699a0a5585c869e7
Martin-Leff <mleff@wrallp.com>
2025-12-19 12:07:44 -0500

    aoc 14 added

---
+++ Advent of Code/2016/aoc_2016_14.py
@@ -0,0 +1,13 @@
+import requests
+import regex as re
+import collections
+from collections import deque, Counter
+from itertools import chain, combinations
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2016/Inputs/aoc_2016_14_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+
+# Part 1
+
+print('Part 2 Answer:', input_text)
