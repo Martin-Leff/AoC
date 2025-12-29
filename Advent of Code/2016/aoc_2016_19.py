0f33888975e4a93ae591fb04a16b2c31e9efe14d
Martin-Leff <mleff@wrallp.com>
2025-12-29 13:38:34 -0500

    Answers Finalized

--- Advent of Code/2016/aoc_2016_19.py
+++ Advent of Code/2016/aoc_2016_19.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 import math
+import collections
 
 from fontTools.misc.cython import returns
 
@@ -10,4 +11,52 @@ input_text = resp.text
 
 # Part 1
 
-print('Part 2 Answer:', input_text)
+elf_list = list(range(1, int(input_text) + 1))
+
+while len(elf_list) > 1:
+    if len(elf_list) % 2 != 0:
+        new_elf_list = elf_list[::2]
+        new_elf_list.pop(0)
+    else:
+        new_elf_list = elf_list[::2]
+    elf_list = new_elf_list
+
+final_elf = elf_list[0]
+
+print('Part 1 Answer:', final_elf)
+
+# Part 2
+
+queue_1 = collections.deque()
+queue_2 = collections.deque()
+
+elf_total = int(input_text)
+
+for i in range(elf_total):
+    queue_1.append(i + 1)
+
+len_q1 = len(queue_1)
+len_q2 = len(queue_2)
+
+l = elf_total
+
+while len_q1 > len_q2:
+    queue_2.append(queue_1.pop())
+    len_q1 -= 1
+    len_q2 += 1
+
+
+while l > 2:
+    queue_2.pop()
+    queue_2.appendleft(queue_1.popleft())
+    len_q1 -= 1
+    if (len_q2 - len_q1) > 1:
+        queue_1.append(queue_2.pop())
+        len_q1 += 1
+        len_q2 -= 1
+    l -= 1
+
+final_elf = queue_1[0]
+
+print('Part 2 Answer:', final_elf)
+
