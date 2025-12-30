d80a88f121337f87dde5728ebe7f7d3ae97d4d22
Martin-Leff <mleff@wrallp.com>
2025-12-30 09:49:37 -0500

    aoc 21 added

--- Advent of Code/2016/aoc_2016_20.py
+++ Advent of Code/2016/aoc_2016_20.py
@@ -1,10 +1,8 @@
 import requests
-import sys
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2016/Inputs/aoc_2016_20_input.txt'
 resp = requests.get(url)
 input_text = resp.text
-data = input_text
 input_text = input_text.splitlines()
 
 # Part 1
