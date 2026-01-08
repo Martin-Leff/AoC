1d6d053388679ca8daa477edc8e8e61e4a2df1a1
Martin-Leff <mleff@wrallp.com>
2026-01-08 15:40:10 -0500

    Answers Finalized

--- Advent of Code/2017/aoc_2017_4.py
+++ Advent of Code/2017/aoc_2017_4.py
@@ -4,7 +4,47 @@ from itertools import combinations
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_4_input.txt'
 resp = requests.get(url)
 input_text = resp.text
+input_text = input_text.splitlines()
 
 # Part 1
 
-print('Part 1 Answer:', input_text)
+count = 0
+
+for line in input_text:
+    line = line.split(' ')
+    fail = False
+    while line and not fail:
+        word = line.pop(0)
+        if word in line:
+            fail = True
+
+    if not fail:
+        count += 1
+
+print('Part 1 Answer:', count)
+
+# Part 2
+
+def check_anagram(string, words):
+    anagram = False
+    for word in words:
+        if sorted(string) == sorted(word):
+            anagram = True
+            return anagram
+
+    return anagram
+
+count = 0
+
+for line in input_text:
+    line = line.split(' ')
+    fail = False
+    while line and not fail:
+        word = line.pop(0)
+        if check_anagram(word, line):
+            fail = True
+
+    if not fail:
+        count += 1
+
+print('Part 2 Answer:', count)
