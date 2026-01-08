bfee7eef240b56d42dcdc68e19a70727df510000
Martin-Leff <mleff@wrallp.com>
2026-01-08 16:59:44 -0500

    aoc 6 added

---
+++ Advent of Code/2017/aoc_2017_6.py
@@ -0,0 +1,10 @@
+import requests
+from itertools import combinations
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_6_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+
+# Part 1
+
+print('Part 1 Answer:', input_text)
