c9986e2245a4bfe1ffe55e57e3a061409b6d0f72
Martin-Leff <mleff@wrallp.com>
2026-03-24 17:29:17 -0400

    Answers Finalized

--- Advent of Code/2017/aoc_2017_9.py
+++ Advent of Code/2017/aoc_2017_9.py
@@ -1,11 +1,58 @@
 import requests
 
-url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_8_input.txt'
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_9_input.txt'
 resp = requests.get(url)
 input_text = resp.text
-input_text = input_text.splitlines()
 
 # Part 1
 
-print('Part 1 Answer:', input_text)
+total_score = 0
+cur_depth = 0
+in_garbage = False
+skip_char = False
 
+for char in input_text:
+    if not in_garbage:
+        if char == '{':
+            cur_depth += 1
+        if char == '}':
+            total_score += cur_depth
+            cur_depth -= 1
+        elif char == '<':
+            in_garbage = True
+    else:
+        if skip_char:
+            skip_char = False
+        elif char == '!':
+            skip_char = True
+        elif char == '>':
+            in_garbage = False
+
+print('Part 1 Answer:', total_score)
+
+# Part 2
+
+garbage_score = 0
+cur_depth = 0
+in_garbage = False
+skip_char = False
+
+for char in input_text:
+    if not in_garbage:
+        if char == '{':
+            cur_depth += 1
+        if char == '}':
+            cur_depth -= 1
+        elif char == '<':
+            in_garbage = True
+    else:
+        if skip_char:
+            skip_char = False
+        elif char == '!':
+            skip_char = True
+        elif char == '>':
+            in_garbage = False
+        else:
+            garbage_score += 1
+
+print('Part 2 Answer:', garbage_score)
