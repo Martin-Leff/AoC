8f0def322000479c3a9d9ab28285851253d4fef2
Martin-Leff <mleff@wrallp.com>
2026-03-24 14:58:07 -0400

    Answers Updated

--- Advent of Code/2017/aoc_2017_7.py
+++ Advent of Code/2017/aoc_2017_7.py
@@ -1,89 +1,81 @@
+import collections
+
 import requests
 import operator
+import networkx as nx
+import matplotlib.pyplot as plt
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_7_input.txt'
 resp = requests.get(url)
 input_text = resp.text
+input_text = input_text.splitlines()
 
 # Part 1
 
-def get_max(block_list):
-    max_index, max_value = max(enumerate(block_list), key=operator.itemgetter(1))
-    return max_index, max_value
+top_list = []
+bottom_list = []
 
-def distribute_block(dis_index, dis_val, block_list):
-    next_index = dis_index + 1
-    if next_index < len(block_list):
-        start_block = block_list[:dis_index] + [0] + block_list[next_index:]
+for line in input_text:
+    if '->' in line:
+        bottom_list.append(line)
     else:
-        start_block = block_list[:dis_index] + [0]
-        next_index = 0
+        top_list.append(line)
 
-    while dis_val:
-        dis_val -= 1
-        next_block = start_block[:next_index] + [start_block[next_index] + 1] + start_block[next_index + 1:]
-        start_block = next_block
-        next_index += 1
-        if next_index >= len(start_block):
-            next_index -= len(start_block)
+holding_list = []
+held_list = []
 
-    return start_block
+for row in bottom_list:
+    holding = row.split('(')[0].strip(' ')
+    holding_list.append(holding)
+    held_temp = row.split('>')[1].split(',')
+    for held_item in held_temp:
+        held_item_stripped = held_item.strip(' ')
+        if held_item_stripped not in held_list:
+            held_list.append(held_item_stripped)
 
+bottom = ''
 
-input_list = input_text.split('\t')
+for item in holding_list:
+    if item not in held_list:
+        bottom = item
 
-block_list = []
-
-for input in input_list:
-    element = int(input)
-    block_list.append(element)
-
-dup_found = False
-iteration_list = [block_list]
-count = 0
-
-while not dup_found:
-    count += 1
-    max_index, max_value = get_max(block_list)
-    new_block_list = distribute_block(max_index, max_value, block_list)
-    if new_block_list not in iteration_list:
-        iteration_list.append(new_block_list)
-    else:
-        dup_found = True
-    block_list = new_block_list
-
-print('Part 1 Answer:', count)
+print('Part 1 Answer:', bottom)
 
 # Part 2
 
-block_list = []
+G = nx.DiGraph()
 
-for input in input_list:
-    element = int(input)
-    block_list.append(element)
+for line in input_text:
+    if '->' in line:
+        base = line.split('->')[0]
+        base_weight = int(base.split('(')[1].strip(') '))
+        base = base.split('(')[0].strip(' ')
+        children_list = line.split('->')[1].strip(' ').split(',')
+        children_list = [x.strip() for x in children_list]
+        G.add_node(base, weight=base_weight)
+        for child in children_list:
+            G.add_edge(base, child)
+    else:
+        node_weight = int(line.split('(')[1].strip(') '))
+        node = line.split('(')[0].strip(' ')
+        G.add_node(node, weight=node_weight)
 
-dup_found = False
-iteration_list = [block_list]
-count = 0
-return_block = []
+ordered = list(nx.topological_sort(G))
 
-end_count = 0
-start_count = 0
+weights = {}
 
-while not dup_found:
-    count += 1
-    max_index, max_value = get_max(block_list)
-    new_block_list = distribute_block(max_index, max_value, block_list)
-    if new_block_list not in iteration_list:
-        iteration_list.append(new_block_list)
-    elif new_block_list == return_block:
-        end_count = count
-        dup_found = True
-    elif not return_block:
-        return_block = new_block_list
-        start_count = count
-    block_list = new_block_list
+for node in reversed(ordered):
+    total = G.nodes[node]['weight']
+    print(node, total)
+    count = collections.Counter(weights[child] for child in G[node])
+    unbal = None
 
-loop_count = end_count - start_count
+    for child in G[node]:
+        if len(count) > 1 and count[weights[child]] == 1:
+            unbal = child
+            break
 
-print('Part 2 Answer:', loop_count)
+        val = weights[child]
+        total += val
+
+
