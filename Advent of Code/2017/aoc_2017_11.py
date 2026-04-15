35172af366c83557c84453c66360d21db2b28ac2
Martin-Leff <mleff@wrallp.com>
2026-04-15 15:46:28 -0400

    Answers Finalized

--- Advent of Code/2017/aoc_2017_11.py
+++ Advent of Code/2017/aoc_2017_11.py
@@ -1,6 +1,5 @@
 import requests
-from functools import reduce
-from operator import xor
+import math
 
 url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_11_input.txt'
 resp = requests.get(url)
@@ -8,5 +7,68 @@ input_text = resp.text
 
 # Part 1
 
+moves = input_text.split(',')
 
-print('Part 1 Answer:', input_text)
+x = 0
+y = 0
+z = 0
+
+
+for move in moves:
+    if move == 'n':
+        z -= 1
+        y += 1
+    elif move == 's':
+        z += 1
+        y -= 1
+    elif move == 'nw':
+        x -= 1
+        y += 1
+    elif move == 'se':
+        x += 1
+        y -= 1
+    elif move == 'ne':
+        x += 1
+        z -= 1
+    else:
+        x -= 1
+        z += 1
+
+distance = (abs(x) + abs(y) + abs(z)) / 2
+
+print('Part 1 Answer:', distance)
+
+# Part 2
+
+x = 0
+y = 0
+z = 0
+
+distance_list = []
+
+for move in moves:
+    if move == 'n':
+        z -= 1
+        y += 1
+    elif move == 's':
+        z += 1
+        y -= 1
+    elif move == 'nw':
+        x -= 1
+        y += 1
+    elif move == 'se':
+        x += 1
+        y -= 1
+    elif move == 'ne':
+        x += 1
+        z -= 1
+    else:
+        x -= 1
+        z += 1
+
+    distance = (abs(x) + abs(y) + abs(z)) / 2
+    distance_list.append(distance)
+
+max_distance = max(distance_list)
+
+print('Part 2 Answer:', max_distance)
