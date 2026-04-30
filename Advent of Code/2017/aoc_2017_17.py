5acb24d127460615942b534092ff11e98f9b5fe8
Martin-Leff <mleff@wrallp.com>
2026-04-30 11:29:48 -0400

    aoc 17 added

---
+++ Advent of Code/2017/aoc_2017_17.py
@@ -0,0 +1,13 @@
+import requests
+from functools import reduce
+from operator import xor
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_17_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+
+# Part 1
+
+
+
+print('Part 2 Answer:', input_text)
