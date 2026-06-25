b2aff42c3a1f005bcc9f505dd04921a10ae4ee3c
Martin-Leff <mleff@wrallp.com>
2026-06-25 12:54:21 -0400

    aoc 19 added

---
+++ Advent of Code/2017/aoc_2017_19.py
@@ -0,0 +1,11 @@
+import requests
+from collections import deque
+
+url = 'https://raw.githubusercontent.com/Martin-Leff/AoC/refs/heads/main/Advent%20of%20Code/2017/Inputs/aoc_2017_19_input.txt'
+resp = requests.get(url)
+input_text = resp.text
+input_text = input_text.splitlines()
+
+# Part 1
+
+print('Part 2 Answer:', input_text)
